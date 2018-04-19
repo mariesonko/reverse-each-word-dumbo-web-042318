25f6208298cd0c5c9799ebd011a569831e4dbe4f
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 13:15:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
+def reverse_each_word(sentence1)
+  sentence1.split.each {|each_word|each_word.reverse}.join(", ")
+end 
+
 def reverse_each_word(sentence2)
   sentence2.split.collect {|each_word|each_word.reverse}.join(" ")
 end
